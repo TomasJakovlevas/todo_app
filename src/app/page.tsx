6e3557796e46578aa42face3922da1371a442744
TomasJakovlevas <tomasjakovlevas@gmail.com>
2023-10-02 20:34:08 +0300

    Moved list logic to new component Added loading fallback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,15 @@
+import TodoList from '@/components/TodoList';
 import NavBar from '@/components/NavBar';
-import TodoItem from '@/components/TodoItem';
-import { prisma } from '@/db';
-import { revalidateTag } from 'next/cache';
+import { Suspense } from 'react';
 
-const getAllTodos = () => {
-  return prisma.todo.findMany();
-};
-
-const toggleTodo = async (id: string, complete: boolean) => {
-  'use server';
-
-  await prisma.todo.update({ where: { id }, data: { complete } });
-};
-
-const deleteTodo = async (id: string) => {
-  'use server';
-
-  await prisma.todo.delete({
-    where: {
-      id: id,
-    },
-  });
-
-  revalidateTag('todo');
-};
-
-const Home = async () => {
-  const todos = await getAllTodos();
-
+const Home = () => {
   return (
     <>
       <NavBar title='Todos' />
       <section className='container max-w-6xl mx-auto'>
-        <ul className='pl-4 flex flex-col gap-2'>
-          {todos.map((todo) => (
-            <TodoItem
-              key={todo.id}
-              {...todo}
-              toggleTodo={toggleTodo}
-              deleteTodo={deleteTodo}
-            />
-          ))}
-        </ul>
+        <Suspense fallback={<div>loading...</div>}>
+          <TodoList />
+        </Suspense>
       </section>
     </>
   );
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,47 @@
+import { prisma } from '@/db';
+import { revalidateTag } from 'next/cache';
+import React from 'react';
+import TodoItem from './TodoItem';
+
+const getAllTodos = async () => {
+  //   await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  return prisma.todo.findMany();
+};
+
+const toggleTodo = async (id: string, complete: boolean) => {
+  'use server';
+
+  await prisma.todo.update({ where: { id }, data: { complete } });
+};
+
+const deleteTodo = async (id: string) => {
+  'use server';
+
+  await prisma.todo.delete({
+    where: {
+      id: id,
+    },
+  });
+
+  revalidateTag('todo');
+};
+
+const TodoList = async () => {
+  const todos = await getAllTodos();
+
+  return (
+    <ul className='pl-4 flex flex-col gap-2'>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          {...todo}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
