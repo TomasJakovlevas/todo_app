196a2d2506e9b5f6a94860ca243539c675350fb2
TomasJakovlevas <tomasjakovlevas@gmail.com>
2023-10-03 21:05:41 +0300

    Created editMode state WIP: handle new form inputs

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { prisma } from '@/db';
+import { useState } from 'react';
 
 type TodoItemProps = {
   id: string;
@@ -23,41 +24,82 @@ const TodoItem = ({
   toggleTodo,
   deleteTodo,
 }: TodoItemProps) => {
+  // Hooks
+  // -- state
+  const [editMode, setEditMode] = useState(false);
+
+  // Functions
+  const handleSubmit = (data: FormData) => {
+    setEditMode(false);
+  };
+
   return (
     <li className='flex flex-col border border-quaternary px-2 py-1 gap-2'>
-      <div className='flex flex-1 gap-2 items-center justify-between'>
-        <div className='flex gap-2 items-center'>
-          <input
-            id={id}
-            type='checkbox'
-            className='cursor-pointer peer'
-            defaultChecked={complete}
-            onChange={(e) => toggleTodo(id, e.target.checked)}
-          />
-          <label
-            htmlFor={id}
-            className='peer-checked:line-through peer-checked:text-slate-500 cursor-pointer text-xl '
-          >
-            {title}
-          </label>
+      <form action={handleSubmit}>
+        <div className='flex flex-1 gap-2 items-center justify-between'>
+          <div className='flex gap-2 items-center'>
+            {editMode ? (
+              <input
+                className='bg-transparent text-xl px-2 outline-none border-b ml-3'
+                type='text'
+                defaultValue={title}
+                name='title'
+              />
+            ) : (
+              <>
+                <input
+                  id={id}
+                  type='checkbox'
+                  className='cursor-pointer peer'
+                  defaultChecked={complete}
+                  onChange={(e) => toggleTodo(id, e.target.checked)}
+                />
+                <label
+                  htmlFor={id}
+                  className='peer-checked:line-through peer-checked:text-slate-500 cursor-pointer text-xl'
+                >
+                  {title}
+                </label>
+              </>
+            )}
+          </div>
+          <div className='text-sm text-secondary'>
+            Last update at:{' '}
+            {updatedAt.toLocaleDateString('lt', {
+              hour: 'numeric',
+              minute: 'numeric',
+            })}
+          </div>
         </div>
-        <div className='text-sm text-secondary'>
-          Last update at:{' '}
-          {updatedAt.toLocaleDateString('lt', {
-            hour: 'numeric',
-            minute: 'numeric',
-          })}
+        <p>{note}</p>
+        <div className='flex justify-end gap-2'>
+          {editMode ? (
+            <>
+              <button
+                className='border border-transparent hover:border-danger hover:text-danger px-2 py-1'
+                onClick={() => deleteTodo(id)}
+                type='button'
+              >
+                Delete
+              </button>
+              <button
+                className='border border-transparent hover:border-tertiary hover:text-tertiary px-2 py-1'
+                type='submit'
+              >
+                Save
+              </button>
+            </>
+          ) : (
+            <button
+              className='border border-transparent hover:border-tertiary hover:text-tertiary px-2 py-1'
+              onClick={() => setEditMode(true)}
+              type='button'
+            >
+              Edit
+            </button>
+          )}
         </div>
-      </div>
-      <p>{note}</p>
-      <div className='flex justify-end'>
-        <button
-          className='border border-transparent hover:border-danger hover:text-danger px-2 py-1'
-          onClick={() => deleteTodo(id)}
-        >
-          Delete
-        </button>
-      </div>
+      </form>
     </li>
   );
 };
