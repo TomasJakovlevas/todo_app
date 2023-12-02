3c86309597f5ac0f0115995578713b7a9b90f1d2
TomasJakovlevas <tomasjakovlevas@gmail.com>
2023-12-02 16:57:57 +0200

    updated about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,11 +4,20 @@ const Page = () => {
   return (
     <>
       <NavBar />
-      <section className='container max-w-6xl mx-auto px-3'>
-        <h1>Dear reader,</h1>
-        <p>
+      <section className='container max-w-6xl mx-auto px-3 py-2 rounded bg-quaternary_l1 dark:bg-secondary'>
+        <h1 className='text-lg mb-2'>Dear reader,</h1>
+        <p className='mb-1'>
           I made this Todo App just to get it out of my system and practice
           Next.js 13 a bit. So please don&apos;t judge too much.
+        </p>
+        <p>
+          What I used?
+          <ul className='list-inside list-disc'>
+            <li>Next.js</li>
+            <li>Tailwind</li>
+            <li>Next-themes</li>
+            <li>Prisma</li>
+          </ul>
         </p>
       </section>
     </>
