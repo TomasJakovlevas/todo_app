e1435e94d024fd7c4b9cd079f01377bff6815591
TomasJakovlevas <tomasjakovlevas@gmail.com>
2023-12-02 20:31:48 +0200

    seperating todos by dates

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -10,6 +10,10 @@ import ToggleFilters from './Filters/ToggleFilters';
 import { useFilteredTodos } from '@/hooks/useTodos';
 import { useSearchParams } from 'next/navigation';
 
+// Helpers
+import { formatDateToISO } from '@/helpers/dateFunctions';
+import { Fragment } from 'react';
+
 type TodoListProps = {
   todos: Array<Todo>;
   toggleTodo: (id: string, complete: boolean) => void;
@@ -38,22 +42,59 @@ const TodoList = ({
   // Functions
   const checkIfItsNew = (id: string) => newId === id;
 
+  const seperateByDates = (arr: Array<Todo>) => {
+    const dateMap: {
+      date?: string;
+      todos?: Todo[];
+    }[] = [];
+
+    arr.forEach((obj: Todo) => {
+      const date = formatDateToISO(new Date(obj.createdAt));
+
+      const foundIndex = dateMap.findIndex((el) => el.date === date);
+
+      if (foundIndex === -1) {
+        dateMap.push({
+          date,
+          todos: [obj],
+        });
+      } else {
+        dateMap[foundIndex].todos?.push(obj);
+      }
+    });
+
+    return dateMap;
+  };
+
+  const seperateByDatesList = seperateByDates(list);
+
   return (
     <>
       <div className='mb-3 '>
         <ToggleFilters filters={filters} />
       </div>
       <ul className='flex flex-col gap-2'>
-        {list.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            {...todo}
-            toggleTodo={toggleTodo}
-            deleteTodo={deleteTodo}
-            updateTodo={updateTodo}
-            isNew={checkIfItsNew(todo.id)}
-          />
-        ))}
+        {seperateByDatesList.length ? (
+          seperateByDatesList.map((obj) => (
+            <Fragment key={obj.date}>
+              <h2 className='text-xl'>{obj.date}</h2>
+              {obj.todos?.map((todo) => (
+                <TodoItem
+                  key={todo.id}
+                  {...todo}
+                  toggleTodo={toggleTodo}
+                  deleteTodo={deleteTodo}
+                  updateTodo={updateTodo}
+                  isNew={checkIfItsNew(todo.id)}
+                />
+              ))}
+            </Fragment>
+          ))
+        ) : (
+          <div className='px-3 py-2 border border-tertiary dark:border-primary rounded text-center'>
+            Empty
+          </div>
+        )}
       </ul>
     </>
   );
---
+++ src/helpers/dateFunctions.ts
@@ -0,0 +1,12 @@
+export const formatDateToISO = (date: Date) => {
+  // Get the year, month, and day from the Date object
+  const year = date.getFullYear();
+  // Months are zero-based, so add 1 to get the correct month
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+
+  // Construct the YYYY-MM-DD string
+  const formattedDate = `${year}-${month}-${day}`;
+
+  return formattedDate;
+};
