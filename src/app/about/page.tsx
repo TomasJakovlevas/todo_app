d05d55bf0fd957b6a18bfe1f10b1ca64d613f869
TomasJakovlevas <tomasjakovlevas@gmail.com>
2023-12-04 21:25:01 +0200

    Update About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,36 +8,57 @@ const Page = () => {
       <section className='container max-w-6xl mx-auto my-3 px-3'>
         <article className='rounded bg-quaternary_l1 dark:bg-secondary py-2 px-3'>
           <h1 className='text-lg mb-3'>
-            Hey, it&apos;s me, your friendle developer Tomas
+            Hey, it&apos;s me, your friendly neighborhood developer Tomas
           </h1>
           <p className='mb-3'>
-            I&apos;m glad that you came all the way to about page, means a lot.
+            I&apos;m glad that you came all the way to the About page, means a
+            lot.
           </p>
 
           <p className='mb-3'>
-            So what it is about? It&apos;s Todo app. Yes. Old good Todo app. I
+            So what is it about? It&apos;s Todo app. Yes. Good old Todo app. I
             figured that I better practice with simple app than bigger more
-            complicated one. That one it&apos;s in progress
+            complicated one. That one is still in progress and not ready to see
+            the light yet.
           </p>
-          <p className='mb-3'>
+          <div className='mb-3'>
             For this app I used:
             <ul className='list-inside list-disc'>
-              <li>Next.js 13</li>
-              <li>Tailwind</li>
-              <li>Next-themes</li>
-              <li>Prisma</li>
-              <li>MongoDB</li>
+              <li>
+                Next.js 13
+                <p className='ml-[24px]'>
+                  New built-in routing system and more{' '}
+                </p>
+              </li>
+              <li>
+                Tailwind <p className='ml-[24px]'>Flexibility and simplicity</p>
+              </li>
+              <li>
+                Prisma
+                <p className='ml-[24px]'>
+                  Simplifies database interactions by providing a clean and
+                  intuitive API
+                </p>
+              </li>
+              <li>
+                MongoDB{' '}
+                <p className='ml-[24px]'>
+                  Flexible schema makes it easy to store data
+                </p>
+              </li>
             </ul>
-          </p>
+          </div>
+
+          <p></p>
 
           <p className='mb-3'>
-            And that&apos;s it. I learned a lot in progress. Using &apos;use
-            server&apos; and &apos;use client&apos; components. Prisma looks
-            very promising. Overall great experience.
+            I learned a lot in progress, and realized how much I don&apos;t
+            know. It&apos;s a big world out there. And I&apos;ll continue to
+            expolore it by one Todo at the time.
           </p>
 
           <p>
-            Created by - Tomas Jakovlevas /{' '}
+            Created by - Tomas Jakovlevas /z
             <Link
               href={'https://github.com/TomasJakovlevas'}
               target='_blank'
